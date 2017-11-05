d186a822d6cce7617ef5f1172c2fa39bcae98883
dapn09 <31325026+dapn09@users.noreply.github.com>
2017-11-05 14:31:30 -0700

    changed way of testing

--- nextFitTest.c
+++ nextFitTest.c
@@ -17,37 +17,6 @@ void getutime(struct timeval *t){
 }
 
 int main(){
-  /*
-  void *p1, *p2, *p3, *p4;
-  arenaCheck();
-  p1 = nextFitAllocRegion(254);
-  arenaCheck();
-  p2 = nextFitAllocRegion(25400);
-  arenaCheck();
-  p3 = nextFitAllocRegion(254);
-  arenaCheck();
-  p4 = nextFitAllocRegion(160);
-
-  printf("%8zx %8zx %8zx %8zx\n", p1, p2, p3, p4);
-  arenaCheck();
-  freeRegion(p3);
-  arenaCheck();
-  */
-  /** 
-   * these next two lines check that nextFitAllocRegion keeps    * track of the last allocated block and resumes searches
-   * from it. In the code before we have created the situation
-   * in which there is a block that can fit the request at the
-   * beginning of the arena but since we start search in the 
-   * last block allocated we place the 200 right next to 
-   * the last. This wouldn't happen with firstFitAllocRegion.
-  **/
-  /*
-  p3 = nextFitAllocRegion(200);//added line
-  arenaCheck();
-  */
-  // measure time for 10000 mallocs */
-  
-  
 
   printf("First we check that the arena is empty\n");
   arenaCheck();
