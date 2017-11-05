dd37a1b91de378e631f7d6bc528ffe4a6f5b0b2e
student <student@localhost.localdomain>
2017-11-05 14:30:03 -0700

    changed way of testing nextFit

--- nextFitTest.c
+++ nextFitTest.c
@@ -17,6 +17,7 @@ void getutime(struct timeval *t){
 }
 
 int main(){
+  /*
   void *p1, *p2, *p3, *p4;
   arenaCheck();
   p1 = nextFitAllocRegion(254);
@@ -31,7 +32,7 @@ int main(){
   arenaCheck();
   freeRegion(p3);
   arenaCheck();
-
+  */
   /** 
    * these next two lines check that nextFitAllocRegion keeps    * track of the last allocated block and resumes searches
    * from it. In the code before we have created the situation
@@ -40,19 +41,57 @@ int main(){
    * last block allocated we place the 200 right next to 
    * the last. This wouldn't happen with firstFitAllocRegion.
   **/
-  
+  /*
   p3 = nextFitAllocRegion(200);//added line
   arenaCheck();
+  */
+  // measure time for 10000 mallocs */
+  
+  
 
-  /* measure time for 10000 mallocs */
-    struct timeval t1, t2;
+  printf("First we check that the arena is empty\n");
+  arenaCheck();
+  printf("\nNow we allocate everything in the arena into equal sized blocks (except last one) for simplicity.");
+  //create an array to store the pointers to the allocations
+  void **pointer = (void*) calloc(17, sizeof(void*));
+  for(int i = 0; i<16; i++){
+    if(i < 15)
+      pointer[i] = nextFitAllocRegion(65536);
+    if(i == 15)
+      pointer[i] = nextFitAllocRegion(65280);
+  }
+  arenaCheck();
+
+  printf("\nNow that the blocks have been allocated we free the odd numbered ones\n");
+  
+  for(int j = 1; j<16; j += 2){
+    freeRegion(pointer[j]);
+  }
+  arenaCheck();
+
+  printf("\nNow we will allocate a block of size 200 with nextFit to see where it is placed, it should be placed in the place where we last allocated a block, which is the end\n");
+  void *p = nextFitAllocRegion(200);
+  arenaCheck();
+
+  printf("\nFinally we allocate another block (65536) bigger than the remaining space at the end (65064) to see if search wraps around and starts searching from arena begin again\n");
+  void *p2 = nextFitAllocRegion(65536);
+  arenaCheck();
+
+  printf("\nTime performance:\n");
+  struct timeval t1, t2;
     int i;
+    //free everything before starting test
+    for(int i = 0; i<16; i++){
+      freeRegion(pointer[i]);
+    }
     getutime(&t1);
     for(i = 0; i < 10000; i++)
       if (nextFitAllocRegion(4) == 0) 
 	break;
     getutime(&t2);
     printf("%d nextFitAllocRegion(4) required %f seconds\n", i, diffTimeval(&t2, &t1));
-  
+    
+  printf("\nNextFit function works as expected\n");
+
   return 0;
 }
